f74650236919624f8f1916334b5ca84d6dc1ca39
Aluno <me@me.com>
2014-07-21 08:56:19 -0300

    Task 1.03 finished for oo

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -3,13 +3,18 @@ package br.ufpb.dce.aps.coffeemachine.impl;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
+import br.ufpb.dce.aps.coffeemachine.MockComponentsFactory;
 
 public class MyCoffeeMachine implements CoffeeMachine {
 
-	private int centavos;
-	private int dolar;
+	private int centavos = 0;
+	private int dolar = 0;
 	private ComponentsFactory factory;
 
+	public void init() {
+		factory = new MockComponentsFactory();
+	}
+	
 	public MyCoffeeMachine(ComponentsFactory factory) {
 		this.factory = factory;
 		factory.getDisplay().info("Insert coins and select a drink!");
@@ -17,13 +22,8 @@ public class MyCoffeeMachine implements CoffeeMachine {
 	}
 
 	public void insertCoin(Coin dime) {
-		centavos = dime.getValue() % 100;
-		dolar = dime.getValue() / 100;
-		factory.getDisplay().info("Total: US$ " + dolar+"."+centavos ); //"Total: US$ 0.10"
-		}
+		dolar += dime.getValue() / 100;
+		centavos += dime.getValue() % 100;
+		factory.getDisplay().info("Total: US$ " + dolar + "." + centavos); //"Total: US$ 0.10"
+	}
 }
-
-
-/*Quando uma moeda é inserida, o display precisa ser invocado para exibir o valor total inserido.
-Dica: trate os valores como centavos (inteiros) em vez de dólares (ponto flutuante). Para pegar 
-os valores dos centavos use "total % 100" e para pegar os valores dos dólares use "total / 100".*/
